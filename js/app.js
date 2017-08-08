debf3e8bd1491ae3d6437feb5436ef51ef9d613f
Will Anderson <willanderson@Wills-MacBook-Pro.local>
2017-08-08 10:48:31 -0700

    added: 404 page

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 
   $("#about-btn").click(function(e){
       $("body").toggleClass("menu--active");
+      $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
       e.preventDefault();
   })
 
   $("#page-content").click(function(e){
   if ( $("body").hasClass( "menu--active" ) ) {
-      $("body").removeClass("menu--active");
-      e.preventDefault();
+          $("body").removeClass("menu--active");
+          $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
+          e.preventDefault();
     }
   });
 
