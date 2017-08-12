ecb61e565f2b855aa76dab4d3e6541459b232cdd
Will Anderson <will@Wills-iMac.local>
2017-08-12 13:57:29 -0700

    fixed permalinks again

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
 
-  $("#about-btn").click(function(e) {
-    $("body").toggleClass("menu--active");
-    $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
-    e.preventDefault();
-  })
+    $("#about-btn").click(function(e) {
+        $("body").toggleClass("menu--active");
+        $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
+        e.preventDefault();
+    })
 
-  $("#page-content").click(function(e) {
-    if ($("body").hasClass("menu--active")) {
-      $("body").removeClass("menu--active");
-      $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
-      e.preventDefault();
-    }
-  });
+    $("#page-content").click(function(e) {
+        if ($("body").hasClass("menu--active")) {
+            $("body").removeClass("menu--active");
+            $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
+            e.preventDefault();
+        }
+    });
+
+    $('.posts').infiniteScroll({
+        // options
+        path: '.pagination__next',
+        append: '.post',
+        status: '.scroller-status',
+        scrollThresold: 100
+    });
 
 
-
-  $('.posts').infiniteScroll({
-    // options
-    path: '.pagination__next',
-    append: '.post',
-    history: true,
-    status: '.scroller-status',
-    scrollThresold: 150
-  });
 });
