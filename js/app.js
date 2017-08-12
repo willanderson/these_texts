d80f856b92234badf59b6da674fac9acc22e2102
Will Anderson <will@Wills-iMac.local>
2017-08-12 14:35:38 -0700

    updated info drawer to make a little smarter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
+    var h = $('#drawer').outerHeight(true);
+    $("#drawer").css("top", h * -1);
+
+
+    $(window).resize(function(){
+      var h = $('#drawer').outerHeight(true)
+      $("#drawer").css("top", h * -1);
+    })
+
     $("#about-btn").click(function(e) {
         $("body").toggleClass("menu--active");
         $('#about-btn').html($('#about-btn').text() == 'Close' ? 'Information' : 'Close')
         e.preventDefault();
     })
+
+
 
     $("#page-content").click(function(e) {
         if ($("body").hasClass("menu--active")) {
